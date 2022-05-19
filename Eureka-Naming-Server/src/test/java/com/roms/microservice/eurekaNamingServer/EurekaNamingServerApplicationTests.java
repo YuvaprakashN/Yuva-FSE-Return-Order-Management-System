88d67a06b0f982a9964730b396d7ed425b662d7b
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2022-05-19 01:20:00 -0400

    Eureka

---
+++ Eureka-Naming-Server/src/test/java/com/roms/microservice/eurekaNamingServer/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roms.microservice.eurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
